56de94eae1d50fa71eaab35e550177e182fd3479
MayurG159 <gulvemayur2000@gmail.com>
2025-02-02 17:34:04 +0530

    changes

--- Frontend.py
+++ Frontend.py
@@ -9,7 +9,7 @@ st.set_page_config(page_title="SQL Chat Assistant", layout="wide")
 
 import os
 
-file_path = '/mount/src/sql_chat_assistant/api_key.txt'
+file_path = 'sql_chat_assistant/api_key.txt'
 with open(file_path) as f:
     api_key = f.read()
 
